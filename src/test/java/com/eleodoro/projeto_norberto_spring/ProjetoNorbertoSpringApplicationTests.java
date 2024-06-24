a213437a6500d9e03a5c7be268f8704654682737
ngduran <norbertogarciaduran@gmail.com>
2024-06-24 12:02:27 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/com/eleodoro/projeto_norberto_spring/ProjetoNorbertoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.projeto_norberto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNorbertoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
